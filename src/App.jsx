7f51a25662c0c66f4afe6dcfc88c2ed80bd1f308
Matteo Giampaolo <doyouthinkysauros@gmail.com>
2024-08-07 13:10:41 +0700

    next and previous page buttons added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,65 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Video from "./components/Video";
 
 function App() {
 
-  const [img,setImg] = useState({});
-  const [playlists, setPlaylists] = useState([])
+  const [playlists, setPlaylists] = useState([]);
+  const [response, setResponse] = useState({});
+  const [pageToken, setToken] = useState("");
 
-  const handleClick = async () => {
+  const fetchData = async (token) => {
     console.log("button clicked.")
     // const partParams = ["snippet","contentDetails","statistics"]
     // const url = `https://youtube.googleapis.com/youtube/v3/channels?part=${partParams.join("%2C")}&id=UCQ59QpOnFZ8byWmE8ayTtaw&key=${import.meta.env.VITE_key}`;
 
-    const url = `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&channelId=UCfR8HhkbpDAwvYxrecNg4Mg&maxResults=25&key=${import.meta.env.VITE_key}`
+    const url = `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&channelId=UCfR8HhkbpDAwvYxrecNg4Mg&pageToken=${token}&maxResults=10&key=${import.meta.env.VITE_key}`
     
     console.log(url)
 
     try{
-
       const response = await fetch(url);
       if (!response.ok){
         throw new Error(`Response status: ${response.status}`)
       }
       const json = await response.json();
+      setResponse(json);
       setPlaylists(json.items);
-      console.log(json.items)
-      // console.log(json.items.length)
-      // console.log(json.items[0].snippet.thumbnails);
-      // setImg(json.items[0].snippet.thumbnails.high)
-
+      console.log("special",json)
+      
     } catch (error) {
       console.error(error.message)
     }
   }
+  
+  useEffect(() => {
+    fetchData(pageToken);
+  },[pageToken])
+
 
   return (
     <div>
       <div className="p-5">
         <h1 className="text-2xl font-bold mb-2">Hello World</h1> 
-        <button className="border px-2 py-1 border-black mb-5 bg-blue-300" onClick={handleClick}>Click me</button>
 
-        {/* <Video imgSrc={"ef"}  /> */}
-        {playlists.map(playlist => {
-          console.log(playlist)
-          return(
-            <div key={playlist.id} className=" bg-white">
-              <Video title={playlist.snippet.title} videoID={playlist.id} thumbnail={playlist.snippet.thumbnails.standard} />
-            </div>
+        <div className="grid grid-cols-5 gap-5">
+          {playlists.map(playlist => {
+            return(
+              <div key={playlist.id} className=" bg-white">
+                <Video title={playlist.snippet.title} videoID={playlist.id} thumbnail={playlist.snippet.thumbnails.standard} />
+              </div>
 
-          )
-        })}
+            )
+          })}
+        </div>
+
+        {response.items !== null ?
+          <div className="flex flex-row gap-3 mt-5">
+            <button onClick={() => {setToken(response.prevPageToken)}} className="disabled:bg-neutral-400 disabled:border-neutral-500 disabled:text-neutral-600 hover:bg-neutral-200 bg-white border-black border px-2 py-1" disabled={response.prevPageToken == null}>Previous</button>
+            <button onClick={() => {setToken(response.nextPageToken)}} className="disabled:bg-neutral-400 disabled:border-neutral-500 disabled:text-neutral-600 hover:bg-neutral-200 bg-white border-black border px-2 py-1" disabled={response.nextPageToken == null}>Next</button>
+          </div>
+        :
+          <></>
+        }
       </div>
     </div>
   )
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 function Video({title,thumbnail,videoID}) {
   return (
-    <div className='p-2 shadow-sm border-black border mb-10'>
+    <div className='p-2 shadow-sm border-black border h-full flex flex-col justify-between'>
+      <div>
         <h1 className='text-2xl font-bold my-3'>{title}</h1>
         <a href={`https://www.youtube.com/playlist?list=${videoID}`}>Link</a>
-        <img className='object-cover w-full' src={thumbnail.url} width={thumbnail.width} height={thumbnail.height} />
+      </div>
+      <img className='object-cover w-full' src={thumbnail.url} width={thumbnail.width} height={thumbnail.height} />
     </div>
   )
 }
